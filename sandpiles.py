c76f74eea3695b9d0a12d44d9aa2ab1bcab8e505
beatrice-erikson <bec12@hampshire.edu>
2019-02-18 20:36:37 -0500

    implement add command

--- sandpiles.py
+++ sandpiles.py
@@ -12,6 +12,11 @@ class level:
 		for layer in self.layers:
 			layer.tick()
 	
+	def add(self,x,y,z,grains):
+		if z >= self.depth or z < 0:
+			return str("layer depth out of range")
+		return self.layers[z].add(x,y,z,grains)
+
 	def display(self):
 		for layer in self.layers:
 			layer.display()
@@ -21,8 +26,8 @@ class layer:
 	def __init__(self, width, height):
 		self.width = width
 		self.height = height
-		self.cells = [x%5 for x in range(width*height)]
-		self.tempcells = list(self.cells)
+		self.tiles = [x%5 for x in range(width*height)]
+		self.temptiles = list(self.tiles)
 	
 	def index(self, x, y):
 		if x < 0 or y < 0 or x >= self.width or y >= self.height:
@@ -37,30 +42,38 @@ class layer:
 		return [up,down,left,right]
 	
 	def tick(self):
-		for i in range(len(self.cells)):
-			if self.cells[i] != None and self.cells[i] > 3:
+		for i in range(len(self.tiles)):
+			if self.tiles[i] != None and self.tiles[i] > 3:
 				self.topple(i%self.width, i//self.width)
-		if self.cells != self.tempcells:
-			self.cells = list(self.tempcells)
+		if self.tiles != self.temptiles:
+			self.tiles = list(self.temptiles)
 			self.tick()
 	
 	def topple(self,x,y):
-		cell = self.index(x,y)
-		pileHeight = self.cells[cell]
+		tile = self.index(x,y)
+		pileHeight = self.tiles[tile]
 		grainsMoving = pileHeight//4
-		self.tempcells[cell] -= grainsMoving*4
+		self.temptiles[tile] -= grainsMoving*4
 		for neighbor in self.neighbors(x,y):
 			if neighbor != None:
-				self.tempcells[neighbor] += grainsMoving
+				self.temptiles[neighbor] += grainsMoving
 	
+	def add(self,x,y,z,grains):
+		tile = self.index(x,y)
+		if not self.tiles[tile]:
+			return "tile out of bounds"
+		self.tiles[tile] += grains
+		self.temptiles[tile] += grains
+		return "added {0} grains of sand to tile {1}, {2} of layer {3}".format(grains,x,y,z)
+
 	def display(self):
 		for y in range(self.height):
-			row = self.cells[y*self.width:(y+1)*self.width]
+			row = self.tiles[y*self.width:(y+1)*self.width]
 			row = " ".join("{0}".format(c) for c in row) + "\n"
 			sys.stdout.write(row)
 
 class gameController:
-	def getInput(self):
+	def getInput(self, table):
 		#Get a round of input
 		command = input("Type next action:")
 		print(command)
@@ -75,14 +88,21 @@ class gameController:
 		elif command == "exit":
 			quit()
 		elif tokens[0] == "add":
-			print("This is the add function, we'll put the input code in here at somepoint")
+			if len(tokens) < 5:
+				print("Not enough args for add. Expected 4, got ", len(tokens)-1)
+			else:
+				try:
+					grains,x,y,z = [int(i) for i in tokens[1:5]]
+					print(table.add(x,y,z,grains))
+				except:
+					print("invalid values for add")
 		else:
 			print("Invalid command, type \"help\" for a list of valid commands")
 
-x=level(4,4,1)
+table=level(4,4,3)
 controller = gameController()
 
 while True:
-	x.tick()
-	x.display()
-	controller.getInput()
+	table.tick()
+	table.display()
+	controller.getInput(table)
